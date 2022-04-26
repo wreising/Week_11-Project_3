46eec158ccc4009589c388b444e3d763c1acc150
wreising <wreising@mac.com>
2022-04-25 17:15:33 -0700

    init

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,16 @@
-import './App.css';
+import './App.css'
+import CssBaseline from '@mui/material/CssBaseline'
+// import Navbar from './components/Navbar'
+// import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      {/* <Navbar /> */}
+      Welcome to Brick Blog
+      {/* <Footer /> */}
+    </>
   );
 }
 
