99a534cf931bd7c4e4658b8f9c08cbedf09d156e
wreising <wreising@mac.com>
2022-04-26 21:34:53 -0700

    new pages

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,26 @@ import CssBaseline from '@mui/material/CssBaseline'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Window from './components/Window'
+import StarWarsSets from './components/StarWarsSets'
+import HarryPotterSets from './components/HarryPotterSets'
+import CastleSets from './components/CastleSets'
 
 function App() {
+  const [pageState, setPageState] = useState({
+    StarWarsSets: true,
+    HarryPotterSets: false,
+    CastleSets: false
+  })
+
   return (
     <>
       <CssBaseline />
-      <Navbar />
+      <Navbar pageState={pageState} setPageState={setPageState} />
       <Window />
+      {pageState.CastleSets ? <Window /> : ''}
+      {pageState.StarWarsSets ? <StarWarsSets /> : ''}
+      {pageState.HarryPotterSets ? <HarryPotterSets /> : ''}
+      {pageState.CastleSets ? <CastleSets /> : ''}
       <Footer />
     </>
   );
---
+++ client/src/components/HarryPotterSets/HarryPotterSets.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HarryPotterSets = () => {
+  return (
+    <div>
+      <h1>Harry Potter Sets</h1>
+      <p>This is the page for Harry Potter Lego sets.</p>
+    </div>
+  )
+}
+
+export default HarryPotterSets
