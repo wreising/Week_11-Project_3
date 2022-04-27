9eff72caa3867d5f27fb9c4e7597d848e757cd86
wreising <wreising@mac.com>
2022-04-27 10:05:02 -0700

    init

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -2,16 +2,38 @@ import React from 'react'
 import Grid from '@mui/material/Grid'
 import './Footer.css'
 
-const Footer = () => {
-  return (
-    <Grid container spacing={3}>
-      <Grid item xs={12}>
-        <p class="footer">
-          © 2022 Brick Blog
-        </p>
-      </Grid>
-    </Grid>
-  )
-}
+// const Footer = () => {
+//   return (
+//     <Grid container spacing={3}>
+//       <Grid item xs={12}>
+//         <p class="footer">
+//           © 2022 Brick Blog
+//         </p>
+//       </Grid>
+//     </Grid>
+//   )
+// }
 
-export default Footer
+// export default Footer
+
+// import React from "react";
+
+const style = {
+  backgroundColor: "#F8F8F8",
+  borderTop: "1px solid #E7E7E7",
+  textAlign: "center",
+  padding: "20px",
+  position: "fixed",
+  left: "0",
+  bottom: "0",
+  height: "60px",
+  width: "100%"
+};
+
+export default function App() {
+  return (
+    <div>
+      <div style={style}>© 2022 - Reising, Sandfer, </div>
+    </div>
+  );
+}
