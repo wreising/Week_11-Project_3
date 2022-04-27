0a665dfff2273d926bd1fc0d2470e2cbae532a25
wreising <wreising@mac.com>
2022-04-27 10:12:53 -0700

    footer at bottom

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,39 +1,24 @@
 import React from 'react'
 import Grid from '@mui/material/Grid'
-import './Footer.css'
-
-// const Footer = () => {
-//   return (
-//     <Grid container spacing={3}>
-//       <Grid item xs={12}>
-//         <p class="footer">
-//           © 2022 Brick Blog
-//         </p>
-//       </Grid>
-//     </Grid>
-//   )
-// }
-
-// export default Footer
-
-// import React from "react";
+import { color } from '@mui/system';
 
 const style = {
-  backgroundColor: "#F8F8F8",
+  backgroundColor: "#2196f3",
   borderTop: "1px solid #E7E7E7",
   textAlign: "center",
-  padding: "20px",
+  padding: "10px",
   position: "fixed",
   left: "0",
   bottom: "0",
-  height: "60px",
-  width: "100%"
+  height: "40px",
+  width: "100%",
+  color: "white"
 };
 
 export default function App() {
   return (
     <div>
-      <div style={style}>© 2022 - Reising, Sandfer, </div>
+      <div style={style}>© 2022 - Reising, Sandfer, Nguy, Hsu</div>
     </div>
   );
 }
