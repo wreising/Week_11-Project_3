91d98d0757ba07a890f3556939e49e5bfee5f053
hsuzach <hsuzach@gmail.com>
2022-04-28 13:32:15 -0700

    database set to brick-blogDB

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const { connect, connection } = require('mongoose');
 
 
 const connectionString =
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/brick-blog';
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/brick-blogDB';
 
 connect(connectionString, {
   useNewUrlParser: true,
