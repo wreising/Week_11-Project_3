7846837e7c7a2bfe0c40cc852e902ba66157c426
wreising <wreising@mac.com>
2022-04-30 14:40:23 -0700

    white

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -8,16 +8,15 @@ const style = {
   textAlign: "center",
   padding: "10px",
   position: "fixed",
-  left: "0",
+  // left: "0",
   bottom: "0",
   height: "40px",
   width: "100%",
   color: "white",
-  borderRadiu: "20px",
-  width: "90%",
+  borderRadius: "20px",
   marginLeft: "auto",
-  marginRight: "auto"
-  color: "black"
+  marginRight: "auto",
+  color: "white"
 };
 
 export default function App() {
